d5e73571adb060422146976e6dd52da9f3ad6c0c
cdubwisdom <cdubwisdom@gmail.com>
2026-07-17 10:57:11 -0500

    Scaffolding

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig, devices} from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    trace: 'on'
+  },
+  projects: [{
+    name: 'chromium',
+    use: {...devices['Desktop Chrome']}
+    }]
+});
